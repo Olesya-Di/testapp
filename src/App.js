9d52b13acc0832a174f3dfb654848fa11a5f6bcb
Olesya-Di <olesya.utipusya@gmail.com>
2023-05-31 17:30:26 +0300

    gp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import FilterLikesPage from './pages/FilterLikesPage';
 
@@ -20,7 +20,7 @@ const App = () => {
     .catch(error => console.log(error))
 
   }, []);
-  
+
   return (
   <>
 
